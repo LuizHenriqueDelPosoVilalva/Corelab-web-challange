5d207b71e422b740c53956e12443cdd0a891f790
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-03-12 19:51:43 -0300

    adicionado joi e validacao no creatpost

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,8 +1,16 @@
 import connect from 'next-connect'
+import Joi from 'joi'
+
+import validate from '../../../lib/middleware/validation'
 
 import { createPost } from '../../../modules/post/post.service'
 
-const post = connect().post((req, res) => {
+const createPostSchema = Joi.object({
+  title: Joi.string().required().max(20),
+  textArea: Joi.string().required().max(100)
+})
+
+const post = connect().post(validate({ body: createPostSchema }), (req, res) => {
   createPost(req.body)
   res.status(200).json({ teste: true })
 })
